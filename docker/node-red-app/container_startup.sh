bf68431527a2ae21d54ee8c05436c4750a2b62f2
crazy <v-muhammadfaisal@nbnco.com.au>
2017-03-08 22:48:20 +1100

    Moved the cloning of the git repo to the shell script from the Dockerfile, as well as created a volume from the git clone location. Added conditions to clone the repo only if it is not already cloned.

--- docker/node-red-app/container_startup.sh
+++ docker/node-red-app/container_startup.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+case ${GIT_CLONE_URL-} in '')
+    echo "$0: Kindly specify Git clone url using --build-arg GIT_CLONE_URL='http://your-git-repo'" >&2;
+    exit 1;;
+esac
+
+if ! git -C ${GIT_CLONE_LOCATION} rev-parse --is-inside-work-tree; then
+    echo "No git repo found. Going to do a git clone..."
+    rm -rf ${GIT_CLONE_LOCATION}/*
+
+    if ! git clone --depth 1 --branch stable $GIT_CLONE_URL $GIT_CLONE_LOCATION; then
+        echo "Exiting...failed to clone the git repo..."
+        rm -rf ${GIT_CLONE_LOCATION}/*
+        exit 1;
+    fi
+fi
+
+if ! git -C ${GIT_CLONE_LOCATION} rev-parse --is-inside-work-tree; then
+    echo "Exiting...no git repo found"
+    exit 1;
+fi
+
 if "${UPDATE_APP:-false}"; then
     echo "Updating the app..."
     git -C ${GIT_CLONE_LOCATION} fetch origin "+refs/tags/stable:refs/tags/stable"

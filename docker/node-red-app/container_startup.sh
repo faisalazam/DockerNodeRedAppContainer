f887766b9bb4a7ea7fe8904cc71f37f2e7aed964
crazy <v-muhammadfaisal@nbnco.com.au>
2017-03-30 09:37:09 +1100

    Updated "container_startup.sh" script to add a new additional task to apply database migrations. DB migrations will kick in only when the file "${GIT_CLONE_LOCATION}/config/database.json" exists as well as the "${GIT_CLONE_LOCATION}/migrations" exists. Database.json is supposed to contain the db configuration, where as migration directory will contain the migrations to apply to db.
    DB-migrate is the chosen tool to apply the db migrations. Please refer "https://db-migrate.readthedocs.io/en/latest/" for more info.

--- docker/node-red-app/container_startup.sh
+++ docker/node-red-app/container_startup.sh
@@ -9,25 +9,37 @@ if ! git -C ${GIT_CLONE_LOCATION} rev-parse --is-inside-work-tree; then
     echo "No git repo found. Going to do a git clone..."
     rm -rf ${GIT_CLONE_LOCATION}/*
 
-    if ! git clone --depth 1 --branch stable $GIT_CLONE_URL $GIT_CLONE_LOCATION; then
-        echo "Exiting...failed to clone the git repo..."
+    if ! git clone --depth 1 --branch stable ${GIT_CLONE_URL} ${GIT_CLONE_LOCATION}; then
+        echo "[ERROR] Exiting...failed to clone the git repo..."
         rm -rf ${GIT_CLONE_LOCATION}/*
         exit 1;
     fi
 fi
 
 if ! git -C ${GIT_CLONE_LOCATION} rev-parse --is-inside-work-tree; then
-    echo "Exiting...no git repo found"
+    echo "[ERROR] Exiting...no git repo found"
     exit 1;
 fi
 
 if "${UPDATE_APP:-false}"; then
     echo "Updating the app..."
-    git -C ${GIT_CLONE_LOCATION} fetch origin "+refs/tags/stable:refs/tags/stable"
-    echo "The app has been updated..."
+    if ! git -C ${GIT_CLONE_LOCATION} fetch origin "+refs/tags/stable:refs/tags/stable"; then
+        echo "[ERROR] The app has NOT been updated..."
+        exit 1;
+    else
+        echo "The app has been updated..."
+    fi
 fi
 
 npm -C ${GIT_CLONE_LOCATION} install
+
+if [ -f ${GIT_CLONE_LOCATION}/config/database.json ] && [ -d ${GIT_CLONE_LOCATION}/migrations ]; then
+    echo "Applying database migrations..."
+    if ! ${GIT_CLONE_LOCATION}/node_modules/db-migrate/bin/db-migrate up --config ${GIT_CLONE_LOCATION}/config/database.json -m ${GIT_CLONE_LOCATION}/migrations; then
+        echo "[ERROR] Database migration failed..."
+        exit 1;
+    fi
+fi
 
 sed -i "s|#{APP_LOCATION}|${GIT_CLONE_LOCATION}|g" ${USER_DIR}/${APP_STARTUP_YML}
 
